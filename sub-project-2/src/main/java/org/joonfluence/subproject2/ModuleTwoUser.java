f1afd7e4d6dcc1d25c092d774b49882d061c1066
joonho lee <joonfluence.dev@gmail.com>
2022-10-20 22:38:51 +0900

    link common module to other module

---
+++ sub-project-2/src/main/java/org/joonfluence/subproject2/ModuleTwoUser.java
@@ -0,0 +1,10 @@
+package org.joonfluence.subproject2;
+
+import org.joonfluence.common.CommonEntity;
+
+public class ModuleTwoUser extends CommonEntity {
+
+    public ModuleTwoUser(String name) {
+        super(name);
+    }
+}
